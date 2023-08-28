85159e6a6dc76abd0e8fd86593f85b53ba57e292
Shanice Griffin <shanicegriffin@Shanices-MacBook-Air.local>
2023-08-27 20:37:05 -0400

    added nav

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  return (
+    <nav>
+        <h2>
+          Youtube
+        </h2>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default NavBar;
