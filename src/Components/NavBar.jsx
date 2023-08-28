7bf549d8a0bd12f537233501cfec1ab2a7479add
Anile Choice <anilechoice@pursuit.orggit config -- global user.nameresetcd ~git config --global user.name>
2023-08-28 14:22:57 -0400

    added margin to search button and youtube logo

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
   return (
     <nav className= "nav .bg-danger fixed-top border-bottom">
         <h2>
-          Youtube
+        <img class="youtube-logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCa4EDbkI8ATSXs7s-ovSP2cX_Qfw06aSRWA&usqp=CAU" alt="youtube-logo" />
         </h2>
       <ul className="row align-items-start">
         <li class="nav-link col">
