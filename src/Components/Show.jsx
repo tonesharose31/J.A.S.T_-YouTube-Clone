4b39399d40243cdfacd44500e140f6ec37725393
Shanice Griffin <shanicegriffin@Shanices-MacBook-Air.local>
2023-08-28 21:16:28 -0400

    fixing video function

--- src/Components/Show.jsx
+++ src/Components/Show.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Show = () => {
+    const { videoId } = useParams();
     return (
         <div>
-            
+            <h1>Video Show Page</h1>
+            <p>Video ID: {videoId}</p>
         </div>
     );
 }
