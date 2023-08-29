ee3c3a512633f54aa4230d7ebc3d891dddf21a8d
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-08-29 19:23:00 -0400

    added some bootstrap css to the homepage

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,39 +1,40 @@
 import React, { useState } from "react";
 import VideoCard from "./VideoCard";
+import VideoList from "./VideoList"
 
-const SearchBar = ({URL}) => {
+import React, { useState } from "react";
 
+const SearchBar = ({ URL }) => {
   const [searchQuery, setSearchQuery] = useState("");
   const [videos, setVideos] = useState([]);
 
-  const handleSearch = async () => {
+  const handleSearch = async (e) => {
+    e.preventDefault();
+
     try {
       const response = await fetch(
         `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${searchQuery}&maxResults=8&key=${URL}`
       );
       const data = await response.json();
-      console.log(data)
       setVideos(data.items);
     } catch (error) {
-      console.error(`Error fetchig search results:`, error);
+      console.error(`Error fetching search results:`, error);
     }
   };
-// console.log(videos)
+
   return (
-    <div>
+    <form className="d-flex">
       <input
         type="text"
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
         placeholder="Search"
+        className="form-control me-2"
       />
-      <button onClick={handleSearch}>Search</button>
-      <div className="video-list">
-        {videos.map((video) => (
-          <VideoCard key={video.id.videoId} video={video} />
-        ))}
-      </div>
-    </div>
+      <button className="btn btn-primary" onClick={handleSearch}>
+        Search
+      </button>
+    </form>
   );
 };
 
