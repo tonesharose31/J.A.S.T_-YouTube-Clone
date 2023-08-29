2769e259c15767a0ebd4af3f065e0d03f70a641f
Shanice Griffin <shanicegriffin@Shanices-MacBook-Air.local>
2023-08-28 20:54:53 -0400

    added route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SearchBar from "./Components/SearchBar";
 import NavBar from "./Components/NavBar";
 import AboutMe from "./Components/AboutMe";
+import Show from "./Components/Show";
 
 function App() {
   const URL = import.meta.env.VITE_API_KEY;
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<SearchBar URL={URL} />} />
           <Route path="/about" element={<AboutMe />} />
+          <Route path="/video/:videoId" element={<Show/>} />
         </Routes>
       </Router>
     </div>
