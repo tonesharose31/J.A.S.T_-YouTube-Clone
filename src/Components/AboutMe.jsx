33272db5f8d7ebd014ee4f837ca5df85337c1c2f
Shanice Griffin <shanicegriffin@Shanices-MacBook-Air.local>
2023-09-02 19:44:22 -0400

    styling about me page

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -2,29 +2,38 @@ import React from "react";
 
 const AboutMe = () => {
   return (
-      <div className="col">
-        <ul className="contributor-list">
-          <div className="card">
+    <div>
+      <div className="row" style={{ paddingTop: "50px", listStyle: "none" }}>
+        <div className="col">
+          <div className="card mx-4 my-4" style={{ width: "18rem" }}>
             <li className="contributers">
               <img
                 src="src/assets/JASTjoseph.png"
                 alt={`Joseph Carter`}
-                class="card-img-top"
+                className="card-img-top"
               />
               <div className="contributer-details-container">
                 <div className="card-body d-block">
                   <h5 className="card-title">{`Joseph Carter`}</h5>
                   <p className="card-text">Things about me.</p>
-                  <a href="https://www.linkedin.com/in/josephaecarter/" className="card-link">
+                  <a
+                    href="https://www.linkedin.com/in/josephaecarter/"
+                    className="card-link"
+                  >
                     Linkedin
                   </a>
                   <br />
-                  <a href="https://github.com/Joseph-Carter" className="card-link">Github</a>
+                  <a
+                    href="https://github.com/Joseph-Carter"
+                    className="card-link"
+                  >
+                    Github
+                  </a>
                 </div>
               </div>
             </li>
           </div>
-          <div className="card">
+          <div className="card mx-4 my-4" style={{ width: "18rem" }}>
             <li className="contributers">
               <img
                 src="src/assets/JASTshanice.jpeg"
@@ -35,16 +44,26 @@ const AboutMe = () => {
                 <div className="card-body d-block">
                   <h5 className="card-title">{`Shanice Griffin`}</h5>
                   <p className="card-text">Things about me</p>
-                  <a href="https://www.linkedin.com/in/shanicegriffin/" className="card-link">
+                  <a
+                    href="https://www.linkedin.com/in/shanicegriffin/"
+                    className="card-link"
+                  >
                     Linkedin
                   </a>
                   <br />
-                  <a href="https://github.com/shanicegrif" className="card-link">Github</a>
+                  <a
+                    href="https://github.com/shanicegrif"
+                    className="card-link"
+                  >
+                    Github
+                  </a>
                 </div>
               </div>
             </li>
           </div>
-          <div className="card">
+        </div>
+        <div className="col">
+          <div className="card mx-4 my-4" style={{ width: "18rem" }}>
             <li className="contributers">
               <img
                 src="src/assets/JASTtonesha.jpeg"
@@ -55,16 +74,24 @@ const AboutMe = () => {
                 <div className="card-body d-block">
                   <h5 className="card-title">{`Tonesha Rose`}</h5>
                   <p className="card-text">Things about me</p>
-                  <a href="https://www.linkedin.com/in/tonesha-rose-53321a273/" className="card-link">
+                  <a
+                    href="https://www.linkedin.com/in/tonesha-rose-53321a273/"
+                    className="card-link"
+                  >
                     Linkedin
                   </a>
                   <br />
-                  <a href="https://github.com/tonesharose31" className="card-link">Github</a>
+                  <a
+                    href="https://github.com/tonesharose31"
+                    className="card-link"
+                  >
+                    Github
+                  </a>
                 </div>
               </div>
             </li>
           </div>
-          <div className="card">
+          <div className="card mx-4 my-4" style={{ width: "18rem" }}>
             <li className="contributers">
               <img
                 src="src/assets/JASTanile.jpeg"
@@ -75,17 +102,26 @@ const AboutMe = () => {
                 <div className="card-body d-block">
                   <h5 className="card-title">{`Anile Choice`}</h5>
                   <p className="card-text">Things about me</p>
-                  <a href="https://www.linkedin.com/in/anile-choice/" className="card-link">
+                  <a
+                    href="https://www.linkedin.com/in/anile-choice/"
+                    className="card-link"
+                  >
                     Linkedin
                   </a>
                   <br />
-                  <a href="https://github.com/anilechoice" className="card-link">Github</a>
+                  <a
+                    href="https://github.com/anilechoice"
+                    className="card-link"
+                  >
+                    Github
+                  </a>
                 </div>
               </div>
             </li>
           </div>
-        </ul>
+        </div>
       </div>
+    </div>
   );
 };
 
