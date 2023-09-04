692f4c20f3e15b9f33ea4acf3adf8a179693e7dc
Shanice Griffin <shanicegriffin@Shanices-MacBook-Air.local>
2023-09-04 15:47:12 -0400

    fixed the sidebar and the search to work only when the button is clicked

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,37 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { useState } from "react";
 
 const NavBar = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
-    <nav className= "navbar navbar-expand-lg navbar-light bg-light fixed-top border-bottom">
-          <button
+    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top border-bottom">
+      <button
         className="navbar-toggler"
         type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
+        onClick={toggleSidebar}
       >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        
-    <Link to="/" className="navbar-brand">
-        <img 
-        className="youtube-logo" 
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCa4EDbkI8ATSXs7s-ovSP2cX_Qfw06aSRWA&usqp=CAU"
-        alt="youtube-logo" 
-        width="120"
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <Link to="/" className="navbar-brand">
+        <img
+          className={`youtube-logo ${sidebarOpen ? "d-none" : ""}`}
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCa4EDbkI8ATSXs7s-ovSP2cX_Qfw06aSRWA&usqp=CAU"
+          alt="youtube-logo"
+          width="120"
           height="30"
-          />
-          </Link>
-     
-        <div className="collapse navbar-collapse" id="navbarNav">
+        />
+      </Link>
+
+      <div className={`collapse navbar-collapse ${sidebarOpen ? "show" : ""}`} id="navbarNav">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
             <Link to="/" className="nav-link">
-            Home
+              Home
             </Link>
           </li>
           <li className="nav-item">
@@ -39,8 +41,8 @@ const NavBar = () => {
               About
             </Link>
           </li>
-      </ul>
-      <div className="ml-3">
+        </ul>
+        <div className="ml-3">
           <i className="fas fa-video fa-lg mr-2"></i>
           <i className="fas fa-th fa-lg mr-2"></i>
           <i className="fas fa-bell fa-lg mr-2"></i>
@@ -50,4 +52,5 @@ const NavBar = () => {
     </nav>
   );
 };
+
 export default NavBar;
